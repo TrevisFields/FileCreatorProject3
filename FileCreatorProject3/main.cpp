08f42274391b0857ef614d37ee9e196e5a00f562
Trevis Jontaé Fields <tfields3@student.rcc.edu>
2017-04-05 09:23:16 -0700

    Initial Commit

---
+++ FileCreatorProject3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FileCreatorProject3
+//
+//  Created by Trevis Jontaé Fields on 4/5/17.
+//  Copyright © 2017 Trevis Jontaé Fields. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
